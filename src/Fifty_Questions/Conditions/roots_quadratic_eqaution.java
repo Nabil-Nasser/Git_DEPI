edcd505395f8f75cce1a9cda2cac74011e48d13c
نبيل <82504675+Nabil-Nasser@users.noreply.github.com>
2025-01-20 22:37:43 +0200

    Update roots_quadratic_eqaution.java

--- src/Fifty_Questions/Conditions/roots_quadratic_eqaution.java
+++ src/Fifty_Questions/Conditions/roots_quadratic_eqaution.java
@@ -1,46 +1,48 @@
 package Fifty_Questions.Conditions;
 
+import java.util.Scanner;
+
 public class roots_quadratic_eqaution {
 
     public static void main(String[] args) {
-        //only work when coefficients are +ve int and need to write 1.
+        Scanner sc = new Scanner(System.in);
 
-        //           012345678
-        String eq = "1x^2+5x+6";
+        System.out.println("\n"+"Enter the coefficients of quadratic equation as in form ax^2 + bx + c = 0");
 
-        String coefficients = "";
+        System.out.print("Enter the value of a : ");
 
-        for (int i = 0; i <= eq.length()-1 ; i++) {
-            if (i == 0 || i==5 || i==8)
-            {
-                //add eq.charAt(0), eq.charAt(3), eq.charAt(8) as them are the coefficients!!
-                coefficients+=eq.charAt(i);
-            }
+        int a = sc.nextInt();
+
+        System.out.print("Enter the value of b : ");
+
+        int b = sc.nextInt();
+
+        System.out.print("Enter the value of c : ");
+
+        int c = sc.nextInt();
+
+        int decision_parameter = (b*b - (4*a*c));
+
+        double sqrt = Math.sqrt( (Math.pow(b, 2)) - 4*a*c);
+
+        if( decision_parameter > 0)
+        {
+            double x1 =  ( -b + sqrt) / (2*a) ;
+
+            double x2 =  ( -b - sqrt) / (2*a) ;
+
+            System.out.println("The roots of the quadratic equation are: "+x1 + " and " + x2);
         }
 
-        // calculate the roots of the quadratic equation ax^2 + bx + c = 0 is (-b ± √(b^2 - 4ac) / 2a) >> -2,-3 !!
+        else if( decision_parameter == 0)
+        {
 
-        //extract the coefficients from the string.
-        int a = coefficients.charAt(0)-48;
-        int b = coefficients.charAt(1)-48;
-        int c = coefficients.charAt(2)-48;
+            double x = (-b + sqrt) / (2*a);
 
-        // reduce calculations ..
-        double sqrt = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+            System.out.println("There is one real root of this quadratic equation which is: "+ x);
+        }
 
-        double x1 = ( -b + sqrt) /2*a ;
-
-        double x2 = ( -b - sqrt) /2*a;
-
-        System.out.println("The roots of the quadratic equation are: "+x1 + " and " + x2);
-
-        //testing..
-        //System.out.println(  a+ " " + b + " " + c);
-
-        //checking for extracting the coefficients
-        // for (int i = 0; i <= coefficients.length()-1; i++) {
-        //     System.out.print(coefficients.charAt(i));
-        // }
+        else if ( decision_parameter < 0 ) System.out.println("No real Solution");
 
     }
 }
